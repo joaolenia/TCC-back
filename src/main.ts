79287e5164a0c95a991b04e0de69cb239f0b1e67
João Pedro Golenia <127161450+joaolenia@users.noreply.github.com>
2025-09-18 13:01:29 -0300

    Fix:main

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(); // permite requisições externas
-  app.enableCors(); 
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0'); // 0.0.0.0 = todas as interfaces
-  console.log(`Aplicação rodando na porta ${port}`);
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port, '0.0.0.0'); 
   console.log(`Aplicação rodando na porta ${port}`);
 }
 bootstrap();
