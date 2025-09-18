93f773be3875e91c57f991a1a28f51adab189d4d
João Pedro Golenia <127161450+joaolenia@users.noreply.github.com>
2025-09-18 13:23:14 -0300

    Fix: CORS

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ["http://52.90.171.79:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false, 
-  });
+  app.enableCors(); // permite requisições externas
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port, '0.0.0.0'); // 0.0.0.0 = todas as interfaces
   console.log(`Aplicação rodando na porta ${port}`);
 }
 bootstrap();
